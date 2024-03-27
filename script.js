32fbafab6ffa1757d331ba2e69276287aa04112b
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2024-03-27 15:42:50 +0100

    fix score + save in localStorage

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
-import { updateGround, setupGround } from "./ground.js"
-import { updateclouds, setupclouds } from "./clouds.js"
-import { updateDino, setupDino, getDinoRect, setDinoLose } from "./dino.js"
-import { updateCactus, setupCactus, getCactusRects } from "./cactus.js"
+import { updateGround, setupGround } from "./ground.js";
+import { updateclouds, setupclouds } from "./clouds.js";
+import { updateDino, setupDino, getDinoRect, setDinoLose } from "./dino.js";
+import { updateCactus, setupCactus, getCactusRects } from "./cactus.js";
 
-const WORLD_WIDTH = 100
-const WORLD_HEIGHT = 30
-const SPEED_SCALE_INCREASE = 0.00001
+const WORLD_WIDTH = 100;
+const WORLD_HEIGHT = 30;
+const SPEED_SCALE_INCREASE = 0.00001;
 
-const worldElem = document.querySelector("[data-world]")
-const scoreElem = document.querySelector("[data-score]")
-const startScreenElem = document.querySelector("[data-start-screen]")
-const stopScreenElem = document.querySelector("[data-end-screen]")
+const worldElem = document.querySelector("[data-world]");
+const scoreElem = document.querySelector("[data-score]");
+const startScreenElem = document.querySelector("[data-start-screen]");
+const stopScreenElem = document.querySelector("[data-end-screen]");
 
-setPixelToWorldScale()
-window.addEventListener("resize", setPixelToWorldScale)
-document.addEventListener("keydown", handleStart, { once: true })
+setPixelToWorldScale();
+window.addEventListener("resize", setPixelToWorldScale);
+document.addEventListener("keydown", handleStart, { once: true });
 
-let lastTime
-let speedScale
-let score
-let Highscore
+let lastTime;
+let speedScale;
+let score;
+let Highscore = parseInt(localStorage.getItem("Highscore"), 10) || 0;
+
+function refreshHighscore() {
+  document.querySelector("[data-highscore]").textContent =
+    Math.floor(Highscore);
+}
+
+refreshHighscore();
+
 function update(time) {
   if (lastTime == null) {
-    lastTime = time
-    window.requestAnimationFrame(update)
-    return
+    lastTime = time;
+    window.requestAnimationFrame(update);
+    return;
   }
-  const delta = time - lastTime
+  const delta = time - lastTime;
 
-  updateclouds(delta, speedScale)
-  updateGround(delta, speedScale)
-  updateDino(delta, speedScale)
-  updateCactus(delta, speedScale)
-  updateSpeedScale(delta)
-  updateScore(delta)
-  if (checkLose()) return handleLose()
+  updateclouds(delta, speedScale);
+  updateGround(delta, speedScale);
+  updateDino(delta, speedScale);
+  updateCactus(delta, speedScale);
+  updateSpeedScale(delta);
+  updateScore(delta);
+  if (checkLose()) return handleLose();
 
-  lastTime = time
-  window.requestAnimationFrame(update)
+  lastTime = time;
+  window.requestAnimationFrame(update);
 }
 
 function checkLose() {
-  const dinoRect = getDinoRect()
-  return getCactusRects().some(rect => isCollision(rect, dinoRect))
+  const dinoRect = getDinoRect();
+  return getCactusRects().some((rect) => isCollision(rect, dinoRect));
 }
-
 
 function isCollision(rect1, rect2) {
   return (
@@ -52,52 +59,51 @@ function isCollision(rect1, rect2) {
     rect1.top < rect2.bottom &&
     rect1.right > rect2.left &&
     rect1.bottom > rect2.top
-  )
+  );
 }
 
 function updateSpeedScale(delta) {
-  speedScale += delta * SPEED_SCALE_INCREASE
+  speedScale += delta * SPEED_SCALE_INCREASE;
 }
 
 function updateScore(delta) {
-  score += delta * 0.01
-  scoreElem.textContent = Math.floor(score)
+  score += delta * 0.01;
+  scoreElem.textContent = Math.floor(score);
 }
 
-
 function handleStart() {
-  lastTime = null
-  speedScale = 1
-  score = 0
-  Highscore = 1000
-  setupclouds()
-  setupGround()
-  setupDino()
-  setupCactus()
-  startScreenElem.classList.add("hide")
-  window.requestAnimationFrame(update)
+  lastTime = null;
+  speedScale = 1;
+  score = 0;
+  setupclouds();
+  setupGround();
+  setupDino();
+  setupCactus();
+  startScreenElem.classList.add("hide");
+  window.requestAnimationFrame(update);
 }
 
 function handleLose() {
-  setDinoLose()
+  setDinoLose();
+  if (score > Highscore) {
+    Highscore = score;
+    refreshHighscore();
+    localStorage.setItem("Highscore", Highscore);
+  }
   setTimeout(() => {
-    document.addEventListener("keydown", handleStart, { once: true })
-    startScreenElem.classList.remove("hide")
-  }, 100)
-}
-
-if (score > Highscore) {
-  Highscore = score;
+    document.addEventListener("keydown", handleStart, { once: true });
+    startScreenElem.classList.remove("hide");
+  }, 100);
 }
 
 function setPixelToWorldScale() {
-  let worldToPixelScale
+  let worldToPixelScale;
   if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
-    worldToPixelScale = window.innerWidth / WORLD_WIDTH
+    worldToPixelScale = window.innerWidth / WORLD_WIDTH;
   } else {
-    worldToPixelScale = window.innerHeight / WORLD_HEIGHT
+    worldToPixelScale = window.innerHeight / WORLD_HEIGHT;
   }
 
-  worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`
-  worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`
+  worldElem.style.width = `${WORLD_WIDTH * worldToPixelScale}px`;
+  worldElem.style.height = `${WORLD_HEIGHT * worldToPixelScale}px`;
 }
